6f1250de1b45ef8ef6b9550f4e3ce06d4d9be262
Sheik Ahamed <sdsheikahamed@gmail.com>
2025-04-05 13:36:46 +0530

    small Changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -49,7 +49,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "464dca46b5f3157080f4db918ec8727f
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "5540a99a2f2859d8d63d3fa17251b5da",
+"flutter_bootstrap.js": "98fd366c794e4b437ab77d467b4f7560",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
