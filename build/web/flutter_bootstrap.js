09b287f376278416a8f0d8702c1bbf0e571c54f0
Sheik Ahamed <sdsheikahamed@gmail.com>
2025-04-06 09:09:41 +0530

    Small Fixes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2089837832"
+    serviceWorkerVersion: "2133367264"
   }
 });
